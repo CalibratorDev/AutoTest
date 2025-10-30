ce5c9027bc0d383f6502ab274a992e8de07ac786
QA Student Morning <qastudentmorning@TT208-11.local>
2025-10-30 09:02:37 -0600

    Change to the positive test, turning it into both positive and negative

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -6,7 +6,7 @@ describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open()
 
-        await LoginPage.login('standard_user', 'secret_sauce')
+        await LoginPage.login('visual_user', 'secret_sauce')
         await expect(SecurePage.appLogo).toBeExisting()
         await expect(SecurePage.appLogo).toHaveText(
             expect.stringContaining('Swag Labs')
